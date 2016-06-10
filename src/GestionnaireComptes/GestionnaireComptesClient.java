3ce0b2ba1111c5380bc296c2785fb9d0860a558b
LeaMiage <lea.cheoux@gmail.com>
2016-06-10 21:45:02 +0200

    Menu Accueil et RH Ajouter dans la configuration "Accueil" ou "RH" en argument

--- src/GestionnaireComptes/GestionnaireComptesClient.java
+++ src/GestionnaireComptes/GestionnaireComptesClient.java
@@ -34,8 +34,6 @@ public class GestionnaireComptesClient {
 
 	        // Recherche aupres du naming service
 	        org.omg.CORBA.Object distantCreationCompte = nameRoot.resolve(nameToFind);
-	        System.out.println("Objet '" + idObj + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantCreationCompte));
 	        
 	        // Casting des objets CORBA
 	        GestionEntreeSortie.CreationCompte creationCompte = GestionEntreeSortie.CreationCompteHelper.narrow(distantCreationCompte);
@@ -67,8 +65,6 @@ public class GestionnaireComptesClient {
 	        
 	        // Recherche aupres du naming service
 	        org.omg.CORBA.Object distantGestionSalaries = nameRoot.resolve(nameToFind2);
-	        System.out.println("Objet '" + idObj2 + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantGestionSalaries));
         
 	        // Casting des objets CORBA
 	        GestionEntreeSortie.GestionSalaries gestionSalaries = GestionEntreeSortie.GestionSalariesHelper.narrow(distantGestionSalaries);
@@ -98,8 +94,6 @@ public class GestionnaireComptesClient {
 	        
 	        // Recherche aupres du naming service
 	        org.omg.CORBA.Object distantGestionEmpreinte = nameRoot.resolve(nameToFind3);
-	        System.out.println("Objet '" + idObj3 + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantGestionEmpreinte));
         
 	        // Casting des objets CORBA
 	        GestionEntreeSortie.GestionEmpreinte gestionEmpreinte = GestionEntreeSortie.GestionEmpreinteHelper.narrow(distantGestionEmpreinte);
@@ -112,20 +106,26 @@ public class GestionnaireComptesClient {
 		return null;
 	}
 	
-	public void creerCP(String nomP, String prenomP, String mdp, String photoP) throws CleInconnue, PersonneExistante, ChampVide{
+	public int creerCP(String nomP, String prenomP, String mdp, String photoP) throws CleInconnue, PersonneExistante, ChampVide{
+		int idP=-1;
 		String [] args = {};
 		CreationCompte creationCompte = GestionnaireComptesClient.getCreationCompte(args);
-		creationCompte.creerCP(nomP, prenomP, mdp, photoP, Utils.Utils.cleApi);
+		idP = creationCompte.creerCP(nomP, prenomP, mdp, photoP, Utils.Utils.cleApi);
+		
+		return idP;
 	}
-	public void creerCT(String nomP, String prenomP, String mdp, String photoP) throws CleInconnue, PersonneExistante, ChampVide{
+	public int creerCT(String nomP, String prenomP, String mdp, String photoP) throws CleInconnue, PersonneExistante, ChampVide{
+		int idP=-1;
 		String [] args = {};
 		CreationCompte creationCompte = GestionnaireComptesClient.getCreationCompte(args);
-		creationCompte.creerCT(nomP, prenomP, mdp, photoP, Utils.Utils.cleApi);
+		idP = creationCompte.creerCT(nomP, prenomP, mdp, photoP, Utils.Utils.cleApi);
+		return idP;
 	}
-	public void rechercherSalarie(int idPersonne) throws PersonneInconnue, CleInconnue{
+	public IdentiteCollaborateur rechercherSalarie(int idPersonne) throws PersonneInconnue, CleInconnue{
 		String [] args = {};
 		GestionSalaries gestionSalarie = GestionnaireComptesClient.getGestionSalaries(args);
-		gestionSalarie.rechercherSalarie(idPersonne, Utils.Utils.cleApi);
+		IdentiteCollaborateur identite = gestionSalarie.rechercherSalarie(idPersonne, Utils.Utils.cleApi);
+		return identite;
 	}	
 	public void verifierPersonne(int idPersonne) throws PersonneInconnue, CleInconnue{
 		String [] args = {};
@@ -139,133 +139,163 @@ public class GestionnaireComptesClient {
 		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			// Intialisation de l'orb
-			org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args,null);
-
-	        // Nom de l'objet CORBA
-	        String idObj = "CreationCompte";
-
-	        // Recuperation du naming service
-	        org.omg.CosNaming.NamingContext nameRoot =
-	        		org.omg.CosNaming.NamingContextHelper.narrow(orb.resolve_initial_references("NameService"));
-
-	        // Construction du nom a rechercher
-	        org.omg.CosNaming.NameComponent[] nameToFind = new org.omg.CosNaming.NameComponent[1];
-	         nameToFind[0] = new org.omg.CosNaming.NameComponent(idObj,"");
-
-	        // Recherche aupres du naming service
-	        org.omg.CORBA.Object distantCreationCompte = nameRoot.resolve(nameToFind);
-	        System.out.println("Objet '" + idObj + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantCreationCompte));
-	        
-	        // Casting des objets CORBA
-	        GestionEntreeSortie.CreationCompte creationCompte = GestionEntreeSortie.CreationCompteHelper.narrow(distantCreationCompte);
-	        
-	        /*
-	        System.out.println(creationCompte.creerCP("Chéoux", "Léa", "photoLea", "lcheoux", Utils.Utils.cleApi));
-	        System.out.println(creationCompte.creerCP("Chevalier", "Théo", "photoTheo", "tchevalier", Utils.Utils.cleApi));
-	        System.out.println(creationCompte.creerCP("Movia", "Bastien", "photoBastien", "bmovia", Utils.Utils.cleApi));			
-	        */
-	        
-	        //*****************************************************//
-	        
-	        // Nom de l'objet CORBA
-	        String idObj2 = "GestionSalaries";
-	        
-	        // Construction du nom a rechercher
-	        org.omg.CosNaming.NameComponent[] nameToFind2 = new org.omg.CosNaming.NameComponent[1];
-	        nameToFind2[0] = new org.omg.CosNaming.NameComponent(idObj2,"");
-	        
-	        // Recherche aupres du naming service
-	        org.omg.CORBA.Object distantGestionSalaries = nameRoot.resolve(nameToFind2);
-	        System.out.println("Objet '" + idObj2 + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantGestionSalaries));
-        
-	        // Casting des objets CORBA
-	        GestionEntreeSortie.GestionSalaries gestionSalaries = GestionEntreeSortie.GestionSalariesHelper.narrow(distantGestionSalaries);
-	        
-	        /*IdentiteCollaborateur ic = gestionSalaries.rechercherSalarie(3, Utils.Utils.cleApi);
-	        System.out.println(ic.idPersonne + " " + ic.nomP + " " + ic.prenomP); 
-	        */
-	        //*****************************************************//
-	        
-	        // Nom de l'objet CORBA
-	        String idObj3 = "GestionEmpreinte";
-	        
-	        // Construction du nom a rechercher
-	        org.omg.CosNaming.NameComponent[] nameToFind3 = new org.omg.CosNaming.NameComponent[1];
-	        nameToFind3[0] = new org.omg.CosNaming.NameComponent(idObj3,"");
-	        
-	        // Recherche aupres du naming service
-	        org.omg.CORBA.Object distantGestionEmpreinte = nameRoot.resolve(nameToFind3);
-	        System.out.println("Objet '" + idObj3 + "' trouve aupres du service de noms. IOR de l'objet :");
-	        System.out.println(orb.object_to_string(distantGestionEmpreinte));
-        
-	        // Casting des objets CORBA
-	        GestionEntreeSortie.GestionEmpreinte gestionEmpreinte = GestionEntreeSortie.GestionEmpreinteHelper.narrow(distantGestionEmpreinte);
-	        
-	        gestionEmpreinte.modifierEmpreinte(0, "empreinte", Utils.Utils.cleApi);
-	        
-		}catch (CleInconnue cleInconnue){
-			System.out.println(cleInconnue.message);
-		}catch (ChampVide champVide) {
-			System.out.println(champVide.message);
-		}catch (PersonneInconnue personneInconnue){
-			System.out.println(personneInconnue.message);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}	
+		GestionnaireComptesClient gestionnaireComptesClient = new GestionnaireComptesClient();
+		gestionnaireComptesClient.menuPrincipal(args);
 	}
 	
-public void menuPrincipal() {
+	public void menuPrincipal(String[] args) {
 		
+		IdentiteCollaborateur identite;
+		String nomP, prenomP, mdp, photoP;
 		int idPersonne;
-		String mdp;
-		
 		// Main du gestionnaire d'empreinte
 
 		Scanner sc = new Scanner(System.in);
 		String str = "1";
 		
-		while(!str.equals("0"))
-		{
-			
-			System.out.println("Bienvenue sur le menu Gestionnaire de Comptes (Accueil). Veuillez vous authentifier pour gérer votre empreinte. (0 pour quitter)");
-			
-			System.out.println("1. Créer compte temporaire\n2. Supprimer une empreinte\n");
-			
-			str = sc.nextLine();
-			
-			/*switch (str){
-			case "1":
-				System.out.println("Entrez votre identifiant");
-				str=sc.nextLine();
-				idPersonne = Integer.parseInt(str);
-				
-				System.out.println("Entrez votre mot de passe");
-				str=sc.nextLine();
-				mdp = str;
+		if (args[0].equals("Accueil")) {
+			while(!str.equals("0"))
+			{
 				System.out.println("\n");
-				try {
-					authentifierCompte(idPersonne, mdp);
-					menuGestionnaireEmpreinte(idPersonne);
-				} catch (ErreurAuthentification erreurAuthentification) {
-					System.out.println(erreurAuthentification.message);
-					str = "1";
-				} catch (CleInconnue cleInconnue) {
-					System.out.println(cleInconnue.message);
-					str = "0";
-				}				
-				break;
+				System.out.println("Bienvenue sur le menu Gestionnaire de Comptes (Accueil). Veuillez choisir l'action à réaliser. (0 pour quitter)");
 				
-			default:
-				System.out.println(str);
-				break;
+				System.out.println("1. Créer compte temporaire\n2. Supprimer une empreinte\n");
+				
+				str = sc.nextLine();
+				
+				switch (str){
+				case "1":
+					System.out.println("Nom du collaborateur");
+					str=sc.nextLine();
+					nomP = str;
+					
+					System.out.println("Prénom du collaborateur");
+					str=sc.nextLine();
+					prenomP = str;
+					
+					System.out.println("Mot de passe du collaborateur");
+					str=sc.nextLine();
+					mdp = str;
+					
+					System.out.println("Photo du collaborateur");
+					str=sc.nextLine();
+					photoP = str;
+					System.out.println("\n");
+					try {
+						int id = creerCT(nomP, prenomP, mdp, photoP);
+						System.out.println("Le collaborateur "+prenomP+" "+nomP+" a bien été créé, son identifiant est le "+id);
+					} catch (CleInconnue cleInconnue) {
+						System.out.println(cleInconnue.message);
+						str="0";
+					} catch (PersonneExistante personneExistante) {
+						System.out.println(personneExistante.message);
+						str="0";
+					} catch (ChampVide champVide) {
+						System.out.println(champVide.message);
+						str="1";
+					}
+									
+					break;
+				case "2":
+					
+					System.out.println("Saisir l'identifiant du collaborateur pour lequel supprimer l'empreinte");
+					str=sc.nextLine();
+					idPersonne = Integer.parseInt(str);
+					System.out.println("\n");
+					try {
+						supprimerEmpreinte(idPersonne);
+						System.out.println("L'empreinte a bien été supprimée.");
+						str="1";
+					} catch (PersonneInconnue personneInconnue) {
+						System.out.println(personneInconnue.message);
+						str="2";
+					} catch (CleInconnue cleInconnue) {
+						System.out.println(cleInconnue.message);
+						str="0";
+					}
+						
+					break;
+					
+				default:
+					System.out.println(str);
+					break;
+				}
 			}
-		*/	
+			System.out.println("\nA bientôt");
 		}
-		System.out.println("\nA bientôt");
+		if (args[0].equals("RH")) {
+			while(!str.equals("0"))
+			{
+				System.out.println("\n");
+				System.out.println("Bienvenue sur le menu Gestionnaire de Comptes (Ressources Humaines). Veuillez choisir l'action à réaliser. (0 pour quitter)");
+				
+				System.out.println("1. Créer compte permanent\n2. Rechercher un collaborateur\n");
+				
+				str = sc.nextLine();
+				
+				switch (str){
+				case "1":
+					System.out.println("Nom du collaborateur");
+					str=sc.nextLine();
+					nomP = str;
+					
+					System.out.println("Prénom du collaborateur");
+					str=sc.nextLine();
+					prenomP = str;
+					
+					System.out.println("Mot de passe du collaborateur");
+					str=sc.nextLine();
+					mdp = str;
+					
+					System.out.println("Photo du collaborateur");
+					str=sc.nextLine();
+					photoP = str;
+					System.out.println("\n");
+					try {
+						int id = creerCP(nomP, prenomP, mdp, photoP);
+						System.out.println("Le collaborateur permanent "+prenomP+" "+nomP+" a bien été créé, son identifiant est le "+id);
+					} catch (CleInconnue cleInconnue) {
+						System.out.println(cleInconnue.message);
+						str="0";
+					} catch (PersonneExistante personneExistante) {
+						System.out.println(personneExistante.message);
+						str="0";
+					} catch (ChampVide champVide) {
+						System.out.println(champVide.message);
+						str="1";
+					}
+									
+					break;
+				case "2":
+					
+					System.out.println("Saisir l'identifiant du collaborateur à rechercher");
+					str=sc.nextLine();
+					idPersonne = Integer.parseInt(str);
+					System.out.println("\n");
+					try {
+						identite = rechercherSalarie(idPersonne);
+						System.out.println("Information du collaborateur "+idPersonne+" :");
+						System.out.println("Nom : "+identite.nomP);
+						System.out.println("Prénom : "+identite.prenomP);
+						System.out.println("Photo : "+identite.photoP);
+						str="1";
+					} catch (PersonneInconnue personneInconnue) {
+						System.out.println(personneInconnue.message);
+						str="2";
+					} catch (CleInconnue cleInconnue) {
+						System.out.println(cleInconnue.message);
+						str="0";
+					}
+						
+					break;
+					
+				default:
+					System.out.println(str);
+					break;
+				}
+			}
+			System.out.println("\nA bientôt");
+		}
+		
 	}
 }
