6951ff150da0323e28fac7d98af767329adac768
Théo Chevalier <theo.chevalier11@gmail.com>
2016-06-22 14:54:24 +0200

    NEW CORBALOC OMAGAD

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -2,5 +2,5 @@ package Utils;
 
 public class Utils {
 	public static String cleApi = "secretKey";
-	public static String corbaloc = "corbaloc:iiop:1.2@dhcp-8-7-13.univ-tlse1.fr:2001/NameService";
+	public static String corbaloc = "corbaloc:iiop:1.2@192.168.43.189:2001/NameService";
 }
