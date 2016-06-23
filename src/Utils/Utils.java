70159c02b5103740dc4aed5b308d744ebf51d5af
LeaMiage <lea.cheoux@gmail.com>
2016-06-23 10:24:15 +0200

    corbaloc

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -2,5 +2,5 @@ package Utils;
 
 public class Utils {
 	public static String cleApi = "secretKey";
-	public static String corbaloc = "corbaloc:iiop:1.2@172.17.63.233:2001/NameService";
+	public static String corbaloc = "corbaloc:iiop:1.2@192.168.43.189:2001/NameService";
 }
